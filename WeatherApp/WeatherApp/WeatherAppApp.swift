ba0a4e11aeec33158989f880e1684af0ec74b9b7
RHellgren <rhellgren@icloud.com>
2024-03-16 13:47:06 +0100

    Setup project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Robin Hellgren on 16/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
